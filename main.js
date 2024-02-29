c0930ab6cd37f53aaa228d799729e119a82edae9
Mariana <159836023+MarianOspina@users.noreply.github.com>
2024-02-29 17:48:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+    $(document).ready(function () {
+        $("#buscar").on("click", function (){
+
+            $.ajax({
+                url: "https://pokeapi.co/api/v2/pokemon/"+ $("#texto").val(),
+                
+               
+                contentType: "application/json",
+               
+                success: function (data) {
+                    console.log(data.sprites.other.home.front_default)
+                    
+                    $(".imagen").html(`<img src="${data.sprites.other.home.front_default}">`)
+                }
+            })
+        })
+    })
+    
+   
+    
